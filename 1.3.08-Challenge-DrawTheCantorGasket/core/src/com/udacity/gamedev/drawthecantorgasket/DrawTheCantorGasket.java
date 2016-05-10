05414514fa1dfadf8a1db4b1020c58a434406e15
linucksrox <ericdaly.aplus@gmail.com>
2016-05-10 18:55:49 -0400

    Complete 1.3.08 challenge

--- 1.3.08-Challenge-DrawTheCantorGasket/core/src/com/udacity/gamedev/drawthecantorgasket/DrawTheCantorGasket.java
+++ 1.3.08-Challenge-DrawTheCantorGasket/core/src/com/udacity/gamedev/drawthecantorgasket/DrawTheCantorGasket.java
@@ -20,7 +20,7 @@ public class DrawTheCantorGasket extends ApplicationAdapter {
 
     ShapeRenderer shapeRenderer;
     // TODO: Set a constant for how many recursions to draw. 5 is a good place to start
-    public static final int recursions = 5;
+    public static final int recursions = 6;
 
     @Override
     public void create () {
@@ -40,22 +40,16 @@ public class DrawTheCantorGasket extends ApplicationAdapter {
         shapeRenderer.begin(ShapeType.Filled);
 
         // TODO: Draw a white square matching the bounds
-        int screenWidth = Gdx.graphics.getWidth();
-        int screenHeight = Gdx.graphics.getHeight();
-        int width = screenWidth;
-        int height = screenHeight;
-        if (screenHeight > screenWidth) {
-            height = width;
-        }
-        else {
-            width = height;
-        }
+        float x = bounds.getX();
+        float y = bounds.getY();
+        float size = bounds.getWidth();
+
         shapeRenderer.setColor(Color.WHITE);
-        shapeRenderer.rect((screenWidth - width) / 2, (screenHeight - height) / 2, width, height);
+        shapeRenderer.rect(x, y, size, size);
 
         // TODO: Set the working color to black, and call punchCantorGasket with the bounds
         shapeRenderer.setColor(Color.BLACK);
-
+        punchCantorGasket(x, y, size, recursions);
 
         // TODO: End the batch
         shapeRenderer.end();
@@ -70,11 +64,42 @@ public class DrawTheCantorGasket extends ApplicationAdapter {
     private void punchCantorGasket(float x, float y, float size, int recursions){
         // Note that size means the height and width of the square
         // TODO: Base case, if recursions = 0, return
+        if (recursions == 0) {
+            return;
+        }
 
         // TODO: Draw a black square in the middle square
+        float punchSize = size / 3;
+        float gasketCornerX = x + punchSize;
+        float gasketCornerY = y + punchSize;
+        shapeRenderer.rect(gasketCornerX, gasketCornerY, punchSize, punchSize);
 
         // TODO: Call punchCantorGasket on all 8 other squares
-
+        // go counter clockwise starting in bottom left corner
+        float x1 = x;
+        float y1 = y;
+        float x2 = x + punchSize;
+        float y2 = y;
+        float x3 = x + punchSize * 2;
+        float y3 = y;
+        float x4 = x3;
+        float y4 = y + punchSize;
+        float x5 = x4;
+        float y5 = y + punchSize * 2;
+        float x6 = x2;
+        float y6 = y5;
+        float x7 = x;
+        float y7 = y6;
+        float x8 = x;
+        float y8 = y4;
+        punchCantorGasket(x1, y1, punchSize, recursions - 1);
+        punchCantorGasket(x2, y2, punchSize, recursions - 1);
+        punchCantorGasket(x3, y3, punchSize, recursions - 1);
+        punchCantorGasket(x4, y4, punchSize, recursions - 1);
+        punchCantorGasket(x5, y5, punchSize, recursions - 1);
+        punchCantorGasket(x6, y6, punchSize, recursions - 1);
+        punchCantorGasket(x7, y7, punchSize, recursions - 1);
+        punchCantorGasket(x8, y8, punchSize, recursions - 1);
     }
 
     private Rectangle findLargestSquare(){
